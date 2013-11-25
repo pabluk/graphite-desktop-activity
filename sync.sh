e3c4fcfc24f8160511e726d06ec7f96df3c57e27
Pablo SEMINARIO <pabluk@gmail.com>
2013-11-25 21:35:38 +0100

    Directory cleanup

--- sync.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python report.py activity.log
-scp web/index.html web/data.json pabluk@ssh.alwaysdata.com:~/www/activity.seminar.io/
-#scp web/data.json pabluk@ssh.alwaysdata.com:~/www/activity.seminar.io/
